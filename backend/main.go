08f4c6b68341be4e9a759ca45a35707aedbbbc31
James <jamessundaram8@gmail.com>
2025-04-29 14:39:08 +0100

    edited server to accept the production url in cors control origin

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,20 @@ type Server struct {
 
 // Override the default Go Mux to Attach a header to all route handlers to check if the request is coming from our frontend
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	// because we can only set a single origin in the header, we will check what the request's origin is and compare it against a list, because we want the production url and development localhost url to be whitelisted
+	allowedOrigins := []string{
+		"http://localhost:5173",
+		"https://dts-developer-challenge-frontend.up.railway.app",
+	}
+	origin := r.Header.Get("Origin")
+	if origin != "" {
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+	}
 	s.mux.ServeHTTP(w, r)
 }
 
